a82522ff56c454a87e78451f6da224322a4d3866
AlsyahFikar <67843319+AlsyahFikar@users.noreply.github.com>
2024-03-21 22:55:39 +0000

    set homepage for github page deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Alsyah Fikar",
+  title: "My Portfolio",
+  email: "alsyahfikar@gmail.com",
+  gitHub: "AlsyahFikar",
+  instagram: "@alsyahfikar",
+  linkedIn: "Alsyah Fikar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "alsyahfikar",
+  youTube: "alsyahfikar",
 };
 
 const primaryColor = "#4E567E";
